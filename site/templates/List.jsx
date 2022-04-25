62b701a464085921318c926764c4e099c0fea1c9
mikehwagz <mike@selfaware.studio>
2022-04-25 13:45:08 -0400

    feat: mobile list module

--- site/templates/List.jsx
+++ site/templates/List.jsx
@@ -2,13 +2,13 @@ import { h } from 'hyposcript'
 
 export default function List({ data }) {
   return (
-    <div class="px-15 m:px-25 pt-40 pb-100 flex gap-x-20">
-      <div class="w-2/12 pt-19">
+    <div class="px-15 m:px-25 pt-40 pb-60 m:pb-100 m:flex gap-x-20">
+      <div class="m:w-2/12 m:pt-19 mb-20 m:mb-0">
         <h2 class="text-28">{data.title}:</h2>
       </div>
-      <ul class="w-10/12 flex flex-wrap">
+      <ul class="m:w-10/12 flex flex-wrap">
         {data.listItems.map(item => (
-          <li class="w-1/2 font-serif text-38 leading-150 tracking-n2 [break-inside:avoid-column]">
+          <li class="m:w-1/2 font-serif text-31 m:text-38 leading-150 tracking-n2">
             {item}
           </li>
         ))}
