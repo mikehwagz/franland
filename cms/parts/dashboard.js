284f6881838e144679d95e74f32bb666481f97b4
mikehwagz <mike@selfaware.studio>
2022-05-01 19:14:59 -0400

    feat: sanity netlify dash

--- cms/parts/dashboard.js
+++ cms/parts/dashboard.js
@@ -6,10 +6,10 @@ export default {
         title: 'Netlify',
         sites: [
           {
-            title: '<SITE_TITLE>',
-            apiId: '<NETLIFY_API_ID>',
-            buildHookId: '<NETLIFY_BUILD_HOOK_ID>',
-            name: '<NETLIFY_SITE_NAME>',
+            title: 'Production',
+            apiId: '769621ec-5874-4d31-90c8-dec8e3d786b0',
+            buildHookId: '626f13cc7b18fe2674a8ec11',
+            name: 'franland',
           },
         ],
       },
