74b0a97e6bb724cc0ba229afc23b602a96b58c0e
mikehwagz <mike@selfaware.studio>
2022-05-13 12:34:43 -0400

    fix: tweak big text size for mobile

--- site/templates/BigText.jsx
+++ site/templates/BigText.jsx
@@ -3,7 +3,7 @@ import BlockContent from './BlockContent'
 
 export default function BigText({ data }) {
   return (
-    <div class="font-serif text-35 m:text-62 leading-128 m:leading-122 tracking-n2 indent-[6rem] m:indent-[12rem] px-15 m:px-25 pt-40 m:pt-60 pb-60 m:pb-150">
+    <div class="font-serif text-31 m:text-62 leading-128 m:leading-122 tracking-n2 indent-[3rem] m:indent-[12rem] px-15 m:px-25 pt-40 m:pt-60 pb-60 m:pb-150">
       <BlockContent blocks={data.body} />
     </div>
   )
